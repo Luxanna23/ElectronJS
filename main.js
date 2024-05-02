d1354b244cc0340950b5eb7d7e1b5543721deebe
Luxanna23 <yasmineadh@gmail.com>
2024-05-02 10:04:54 +0200

    fixed

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 const { dialog } = require('electron');
-const electron  = require('electron')
-const {app, BrowserWindow, ipcMain, Menu} = electron
+const electron = require('electron')
+const { app, BrowserWindow, ipcMain, Menu } = electron
 const fs = require('fs');
 
 let win;
 let path = null;
 
-app.on('ready', ()=>{
+app.on('ready', () => {
     win = new BrowserWindow({
-        webPreferences : {
-            nodeIntegration : true
+        webPreferences: {
+            nodeIntegration: true
         }
     });
     win.loadFile('index.html');
@@ -17,11 +17,46 @@ app.on('ready', ()=>{
     Menu.setApplicationMenu(menu);
 })
 
+const isMac = process.platform === 'darwin'
+const template = [
+    ...(isMac
+        ? [{
+            label: app.name,
+            submenu: [
+                { role: 'about' },
+            ]
+        }]
+        : []),
+    {
+        label: 'Save',
+        submenu: [
+            {
+                label: "Save",
+                accelerator: "CommandOrControl+S",
+                click: () => {
+                    win.webContents.send('savebutton')
+                }
+            },
+            {
+                label: "Save As",
+                accelerator: "CommandOrControl+Shift+S",
+                click: () => {
+                    path = null;
+                    win.webContents.send('savebutton')
+                }
+            }
+        ]
+    },
+    { role: 'editMenu' },
+    { role: "viewMenu" },
+]
+
+
 ipcMain.on('save', (event, text) => {
     console.log(`${text} from main`);
-    if (path === null){
-        dialog.showSaveDialog(win, {defaultPath: "text.txt"}, (filePath) => {
-            if (filePath){
+    if (path === null) {
+        dialog.showSaveDialog(win, { defaultPath: "text.txt" }, (filePath) => {
+            if (filePath) {
                 path = filePath;
                 fs.writeFile(filePath, text, err => {
                     if (err) {
@@ -31,7 +66,7 @@ ipcMain.on('save', (event, text) => {
                         console.error('saved');
                         win.webContents.send('background', 'green')
                     }
-                });   
+                });
             }
         });
     }
@@ -45,7 +80,7 @@ ipcMain.on('save', (event, text) => {
                 win.webContents.send('background', 'green')
 
             }
-        });   
+        });
     }
 
 })
